2f2a05611d6ab66d56205e146f362b34b1c26e06
seokjun01 <suckjungod@naver.com>
2024-10-10 17:28:37 +0900

    연관관계 편의 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -21,17 +21,34 @@ public class Order {
     @JoinColumn(name = "member_id") //외래 키 이름이 멤버 id가 됨
     private Member member; //멤버와 관계 세팅 , 다대일 관계
 
-    @OneToMany (mappedBy = "order")
+    @OneToMany (mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
+    //order persist할 때, delivery도 같이 persist 해줌
 
     private LocalDateTime orderDate; // 하이버네이트가 알아서 지원해줌
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 ,order, cancel
+
+    //==연관관계 메서드==//
+    public  void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 
 
