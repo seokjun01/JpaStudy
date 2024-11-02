de9af8a69ee8bb139e4a13c16b055456010d7517
seokjun01 <suckjungod@naver.com>
2024-11-02 17:02:08 +0900

    상품 도메인 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { //id값이 없기때문에 새로 생성하는 객체라고 생성
+            em.persist(item);
+        }else {
+            em.merge(item); // 그 후 부터는 업데이트라고 생각
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
